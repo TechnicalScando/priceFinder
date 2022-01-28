277cbcf18998595b6e77a6174f5574771f2d437b
Gary <kikz270@gmail.com>
2022-01-28 09:28:32 -0600

    make price appear on the pricelist and calculated the upper percentage mark

--- priceFinder.js
+++ priceFinder.js
@@ -1,11 +1,26 @@
 const priceInput = document.querySelector('.priceInput')
 const submitButton = document.querySelector('.submitButton')
+const priceList = document.querySelector('.priceList')
+const resetButton = document.querySelector('.resetButton')
 
 let submit = () => {
-    const price = priceInput.value
+    const price = calcPercent(priceInput.value)
     if(price){
-    console.log(`The value is: ${priceInput.value}`)
+    priceList.textContent = price
     }
 }
 
+let reset = () => {
+    priceList.textContent = ''
+    priceInput.value = ''
+}
+
+let calcPercent = (price, percentage) => {
+    numPrice = Number(price)
+    percentage = .03
+    calcPrice = numPrice + (numPrice * percentage)
+    return calcPrice 
+}
+
 submitButton.addEventListener('click', submit)
+resetButton.addEventListener('click', reset)
